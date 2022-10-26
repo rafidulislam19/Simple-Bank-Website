5b6ea5bc209efb5647f9161d85c9e335c7f8d6f4
rafidulislam19 <rafidul.islam19@northsouth.edu>
2022-10-26 22:40:23 +0600

    ban web js login

---
+++ js/login.js
@@ -0,0 +1,23 @@
+//add click event handler with submit button
+document.getElementById('btn-submit').addEventListener('click', function () {
+    //get the email address
+    //remember to use .value to get text from input field
+    const emailField = document.getElementById('user-email');
+    const email = emailField.value;
+
+    //get password
+    const passField = document.getElementById('user-pass');
+    const password = passField.value;
+
+    //dontverify email pass on the client side
+    //verify email,pass
+
+    if (email == 'rafid@gmail.com' && password == 'secretcse') {
+    console.log('valid user');
+}
+    else {
+    console.log('Invalid user');
+}
+
+
+})
