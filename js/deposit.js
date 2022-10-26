9ef64a38e05b26b1070c6d1e3cbfc8b5366dbc57
rafidulislam19 <rafidul.islam19@northsouth.edu>
2022-10-27 03:35:44 +0600

    balance

--- js/deposit.js
+++ js/deposit.js
@@ -1,8 +1,16 @@
 document.getElementById('btn-deposite').addEventListener('click',function(){
     const depositField = document.getElementById('deposit');
-    const depositAmount = depositField.value;
+    const newDepositAmount = depositField.value;
     // console.log(depositAmount);
     const depositTotalElement = document.getElementById('deposit-total');
-    const depositTotal = depositTotalElement.innerText;
-    depositTotalElement.innerText = depositAmount;
+    const previousDepositTotal = depositTotalElement.innerText;
+    currentDepositTotal = parseFloat(newDepositAmount) + parseFloat(previousDepositTotal);
+    depositTotalElement.innerText = currentDepositTotal;
+
+    const balanceTotal = document.getElementById('balance-total');
+    const previousBalanceTotal = balanceTotal.innerText;
+    const currentBalanceTotal = parseFloat(newDepositAmount) + parseFloat(previousBalanceTotal);
+    balanceTotal.innerText = currentBalanceTotal;
+
+    depositField.value = '';
 })
