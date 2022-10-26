adb98966a3479305feed07035a56b4e8d8811304
rafidulislam19 <rafidul.islam19@northsouth.edu>
2022-10-27 04:27:49 +0600

    ...

--- js/withdraw.js
+++ js/withdraw.js
@@ -2,15 +2,32 @@ document.getElementById('btn-withdraw').addEventListener('click',function(){
     const withdrawField = document.getElementById('withdraw');
     const newWithdrawAmount = withdrawField.value;
 
+    
+
+    // if (isNaN(newWithdrawAmount)){
+    //     alert('Please provide a valid number');
+    //     return;
+    // }
+
     const withdrawTotalElement = document.getElementById('withdraw-total');
     const previousWithdrawTotal = withdrawTotalElement.innerText;
-    currentWithdrawTotal = parseFloat(newWithdrawAmount) + parseFloat(previousWithdrawTotal);
-    withdrawTotalElement.innerText = currentWithdrawTotal;
+    
 
     const balanceTotal = document.getElementById('balance-total');
     const previousBalanceTotal = balanceTotal.innerText;
+
+    withdrawField.value = '';
+
+    if (newWithdrawAmount > previousBalanceTotal) {
+        alert('Eto taka nai bhai!');
+        return;
+    }
+
+    const currentWithdrawTotal = parseFloat(newWithdrawAmount) + parseFloat(previousWithdrawTotal);
+    withdrawTotalElement.innerText = currentWithdrawTotal;
+
     const currentBalanceTotal = parseFloat(previousBalanceTotal) - parseFloat(newWithdrawAmount);
     balanceTotal.innerText = currentBalanceTotal;
 
-    withdrawField.value = '';
+    
 })
