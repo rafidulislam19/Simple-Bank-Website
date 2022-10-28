09d68042014224fe887bce29852870c31a77ec6a
rafidulislam19 <rafidul.islam19@northsouth.edu>
2022-10-28 22:10:09 +0600

    updated

--- js/withdraw.js
+++ js/withdraw.js
@@ -1,33 +1,48 @@
-document.getElementById('btn-withdraw').addEventListener('click',function(){
+// add event handler with the withdraw button
+
+document.getElementById('btn-withdraw').addEventListener('click', function () {
+    // get the withdraw amount from input field
     const withdrawField = document.getElementById('withdraw');
-    const newWithdrawAmount = withdrawField.value;
+    const newWithdrawAmountString = withdrawField.value;
+    //converting string input to number
+    const newWithdrawAmount = parseFloat(newWithdrawAmountString);
 
-    
+    //clear input
+    withdrawField.value = '';
 
-    // if (isNaN(newWithdrawAmount)){
-    //     alert('Please provide a valid number');
-    //     return;
-    // }
+    if (isNaN(newWithdrawAmount)) {
+        alert('Please provide a valid amount');
+        return;
+    }
+
+    // get previous withdraw total
 
     const withdrawTotalElement = document.getElementById('withdraw-total');
-    const previousWithdrawTotal = withdrawTotalElement.innerText;
-    
+    const previousWithdrawTotalString = withdrawTotalElement.innerText;
+    const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
 
-    const balanceTotal = document.getElementById('balance-total');
-    const previousBalanceTotal = balanceTotal.innerText;
 
-    withdrawField.value = '';
+
+    // get previous balace total
+    const balanceTotalElement = document.getElementById('balance-total');
+    const previousBalanceTotalString = balanceTotalElement.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceTotalString);
 
     if (newWithdrawAmount > previousBalanceTotal) {
         alert('Eto taka nai bhai!');
         return;
     }
 
-    const currentWithdrawTotal = parseFloat(newWithdrawAmount) + parseFloat(previousWithdrawTotal);
+    //update total withdraw
+    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+
+    // set total withdraw amount
     withdrawTotalElement.innerText = currentWithdrawTotal;
 
-    const currentBalanceTotal = parseFloat(previousBalanceTotal) - parseFloat(newWithdrawAmount);
-    balanceTotal.innerText = currentBalanceTotal;
+    // new balance 
+    const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+    balanceTotalElement.innerText = newBalanceTotal;
 
-    
+    // 
+
 })
